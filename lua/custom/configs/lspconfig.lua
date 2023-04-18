4a02f8faa12071395b3912ecfb173d5f04f4384a
Samuel W <sam@cosmicbagel.com>
2023-04-17 19:35:29 -0600

    csharp working

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -3,9 +3,24 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
 
+-- lspconfig.omnisharp.setup({
+--
+-- })
+
 lspconfig.rust_analyzer.setup({
     on_attach = on_attach,
     capabilities = capabilities,
     filetypes = {"rust"},
     root_dir = lspconfig.util.root_pattern("Cargo.toml"),
+})
+
+lspconfig.csharp_ls.setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+    cmd = { 'csharp-ls' },
+    root_dir = lspconfig.util.root_pattern('*.sln', '*.csproj', '*.fsproj', '.git'),
+    filetypes = { 'cs' },
+    init_options = {
+      AutomaticWorkspaceInit = true,
+    },
 })
