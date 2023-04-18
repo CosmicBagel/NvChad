50fb49166e4382397adf9877e9cac83d30f44a66
Samuel W <sam@cosmicbagel.com>
2023-04-17 19:06:10 -0600

    ruler and powershell

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -1,3 +1,12 @@
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
+
+vim.opt.colorcolumn = "120"
+
+vim.opt.shell= "pwsh.exe -NoLogo"
+vim.opt.shellxquote = ""
+vim.opt.shellcmdflag = "-Command"
+vim.opt.shellquote   = ""
+vim.opt.shellpipe    = "| Out-File -Encoding UTF8 %s"
+vim.opt.shellredir   = "| Out-File -Encoding UTF8 %s"
