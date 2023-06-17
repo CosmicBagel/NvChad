848f4fc97ead83f25341ccc3203bd985a9aa847a
CosmicBagel <>
2023-06-17 09:42:17 -0600

    Using pwsh on windows, bash on macos & linux

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -4,16 +4,19 @@ vim.opt.shiftwidth = 4
 
 vim.opt.colorcolumn = "120"
 
-vim.opt.shell= "pwsh.exe -NoLogo"
-vim.opt.shellxquote = ""
-vim.opt.shellcmdflag = "-Command"
-vim.opt.shellquote   = ""
-vim.opt.shellpipe    = "| Out-File -Encoding UTF8 %s"
-vim.opt.shellredir   = "| Out-File -Encoding UTF8 %s"
+if vim.loop.os_uname().sysname == "Windows" then
+  vim.opt.shell = "pwsh.exe -NoLogo"
+  vim.opt.shellxquote = ""
+  vim.opt.shellcmdflag = "-Command"
+  vim.opt.shellquote = ""
+  vim.opt.shellpipe = "| Out-File -Encoding UTF8 %s"
+  vim.opt.shellredir = "| Out-File -Encoding UTF8 %s"
+else
+  vim.opt.shell = "bash"
+end
 
 vim.wo.relativenumber = true
 
-vim.opt.mousescroll="ver:1,hor:3"
+vim.opt.mousescroll = "ver:1,hor:3"
 
-vim.api.nvim_create_user_command('LL', 'Lazy load all', {})
-
+vim.api.nvim_create_user_command("LL", "Lazy load all", {})
