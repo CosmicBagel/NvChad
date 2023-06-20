114d5cdb432533a42ee48886af7ce69b2df14c74
CosmicBagel <>
2023-06-19 20:01:52 -0600

    Fixed incorrect os detection for use of powershell

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -4,7 +4,9 @@ vim.opt.shiftwidth = 4
 
 vim.opt.colorcolumn = "120"
 
-if vim.loop.os_uname().sysname == "Windows" then
+local sysname = vim.loop.os_uname().sysname
+if sysname == "Windows_NT" then
+  -- using powershell on windows
   vim.opt.shell = "pwsh.exe -NoLogo"
   vim.opt.shellxquote = ""
   vim.opt.shellcmdflag = "-Command"
@@ -12,22 +14,27 @@ if vim.loop.os_uname().sysname == "Windows" then
   vim.opt.shellpipe = "| Out-File -Encoding UTF8 %s"
   vim.opt.shellredir = "| Out-File -Encoding UTF8 %s"
 else
+  -- using bash on macos and linux
   vim.opt.shell = "bash"
+end
+
+if sysname == "Darwin" then
+    vim.opt.mousescroll = "ver:1,hor:3"
 end
 
 vim.wo.relativenumber = true
 
-vim.opt.mousescroll = "ver:1,hor:3"
-
 function dump(o)
-   if type(o) == 'table' then
-      local s = '{ '
-      for k,v in pairs(o) do
-         if type(k) ~= 'number' then k = '"'..k..'"' end
-         s = s .. '['..k..'] = ' .. dump(v) .. ','
+  if type(o) == "table" then
+    local s = "{ "
+    for k, v in pairs(o) do
+      if type(k) ~= "number" then
+        k = '"' .. k .. '"'
       end
-      return s .. '} '
-   else
-      return tostring(o)
-   end
+      s = s .. "[" .. k .. "] = " .. dump(v) .. ","
+    end
+    return s .. "} "
+  else
+    return tostring(o)
+  end
 end
