72e1c0dbf26f530483c324030db6296a695b422c
CosmicBagel <>
2023-06-27 12:55:07 -0600

    Windows omnisharp working

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -2,18 +2,18 @@ local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local configs = require 'lspconfig/configs'
+local configs = require "lspconfig/configs"
 
 -- lspconfig.omnisharp.setup({
 --
 -- })
 
-lspconfig.rust_analyzer.setup({
-    on_attach = on_attach,
-    capabilities = capabilities,
-    filetypes = {"rust"},
-    root_dir = lspconfig.util.root_pattern("Cargo.toml"),
-})
+lspconfig.rust_analyzer.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = { "rust" },
+  root_dir = lspconfig.util.root_pattern "Cargo.toml",
+}
 
 --[[
 https://github.com/razzmatazz/csharp-language-server
@@ -33,52 +33,68 @@ The preferred way to install csharp-ls is with `dotnet tool install --global csh
 --     useModernNet = false,
 -- })
 
-local omnisharp_bin = "/Users/sam/omnisharp/OmniSharp.exe"
-lspconfig.omnisharp.setup({
-    on_attach = on_attach,
-    capabilities = capabilities,
-    cmd = { 'mono', omnisharp_bin },
-    -- useModernNet = false,
-    root_dir = lspconfig.util.root_pattern('*.sln', '*.csproj', '*.fsproj', '.git'),
-    filetypes = { 'cs' },
-    -- init_options = {
-    --   AutomaticWorkspaceInit = true,
-    -- },
-})
+local omnisharp_bin = ""
+local omnisharp_cmd = {}
+
+if vim.loop.os_uname().sysname == "Windows_NT" then
+  omnisharp_bin = "C:\\Users\\samue\\AppData\\Local\\nvim-data\\mason\\packages\\omnisharp-mono\\OmniSharp.exe"
+  omnisharp_cmd = { omnisharp_bin }
+else
+  omnisharp_bin = "/Users/sam/omnisharp/OmniSharp.exe"
+  omnisharp_cmd = { "mono", omnisharp_bin }
+end
+
+lspconfig.omnisharp.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  cmd = omnisharp_cmd,
+  -- useModernNet = false,
+  root_dir = lspconfig.util.root_pattern("*.sln", "*.csproj", "*.fsproj", ".git"),
+  filetypes = { "cs" },
+  -- init_options = {
+  --   AutomaticWorkspaceInit = true,
+  -- },
+}
 
 lspconfig.gopls.setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
-    root_dir = lspconfig.util.root_pattern(".git", "go.mod"),
-    filetypes = { 'go', 'go.mod' },
-    flags = {
-        debounce_text_changes = 150,
+  on_attach = on_attach,
+  capabilities = capabilities,
+  root_dir = lspconfig.util.root_pattern(".git", "go.mod"),
+  filetypes = { "go", "go.mod" },
+  flags = {
+    debounce_text_changes = 150,
+  },
+  settings = {
+    gopls = {
+      gofumpt = true,
+      experimentalPostfixCompletions = true,
+      staticcheck = true,
+      usePlaceholders = true,
     },
-    settings = {
-        gopls = {
-            gofumpt = true,
-            experimentalPostfixCompletions = true,
-            staticcheck = true,
-            usePlaceholders = true,
-        },
-    },
+  },
 }
 
 if not configs.golangcilsp then
- 	configs.golangcilsp = {
-		default_config = {
-			cmd = {'golangci-lint-langserver'},
-			root_dir = lspconfig.util.root_pattern('.git', 'go.mod'),
-			init_options = {
-					command = {
-                        "golangci-lint", "run", "--enable-all", "--disable",
-                        "lll", "--out-format", "json", "--issues-exit-code=1"
-                    };
-			}
-		};
-	}
+  configs.golangcilsp = {
+    default_config = {
+      cmd = { "golangci-lint-langserver" },
+      root_dir = lspconfig.util.root_pattern(".git", "go.mod"),
+      init_options = {
+        command = {
+          "golangci-lint",
+          "run",
+          "--enable-all",
+          "--disable",
+          "lll",
+          "--out-format",
+          "json",
+          "--issues-exit-code=1",
+        },
+      },
+    },
+  }
 end
 
 lspconfig.golangci_lint_ls.setup {
-	filetypes = {'go','gomod'}
+  filetypes = { "go", "gomod" },
 }
