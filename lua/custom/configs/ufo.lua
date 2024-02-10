21c28513567b1bd06f0b672beb0c08832d5cfe07
CosmicBagel <>
2024-02-10 14:07:00 -0700

    Note for indent fallback if ufo fails

--- lua/custom/configs/ufo.lua
+++ lua/custom/configs/ufo.lua
@@ -2,6 +2,7 @@ vim.o.foldcolumn = "1" -- '0' is not bad
 vim.o.foldlevel = 99
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
+-- vim.o.foldmethod = "indent"
 
 require("ufo").setup {
   provider_selector = function(bufnr, filetype, buftype)
