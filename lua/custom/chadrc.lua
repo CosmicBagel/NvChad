f5b0fd1caf34cf187e6b57bed712e227a017cd09
CosmicBagel <>
2024-02-12 16:56:52 -0700

    disabled tabubufline

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -15,6 +15,13 @@ M.plugins = "custom.plugins"
 
 M.mappings = require "custom.mappings"
 
+-- lazyload it when there are 1+ buffers
+M.ui.tabufline = {
+  show_numbers = false,
+  enabled = false,
+  lazyload = true,
+  overriden_modules = nil,
+}
 M.ui.nvdash = {
   load_on_startup = true,
 
